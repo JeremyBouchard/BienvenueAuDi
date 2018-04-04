bba695f5d81d9a72017ef12a0c8b4dfba506e8aa
JermanKell <xbouchenard@gmail.com>
2018-04-04 18:52:31 +0200

    Nouvelles classes + comments

--- src/Controllers/TimeHandler.java
+++ src/Controllers/TimeHandler.java
@@ -3,15 +3,38 @@ package Controllers;
 import java.time.LocalDateTime;
 
 public class TimeHandler implements Runnable{
+	/**
+	 * Reference of the current time
+	 */
 	private LocalDateTime date = null;
+	/**
+	 * State of this variable will change if the time delay of 2 hours will be elapsed
+	 */
+	private static boolean ValUpdate = false;
 	
+	/**
+	 * Constructor of the class
+	 */
 	public TimeHandler() {
 		date = LocalDateTime.now();
 	}
 	
+	/**
+	 * @author Xavier Bouchenard
+	 * Runs a thread which will check the time duration is elapsed and do the correct actions
+	 */
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		
 	}	
+	
+	/**
+	 * @author Xavier Bouchenard
+	 * @return ValUpdate	State of the update to do
+	 * Returns the state of the variable which shows if the update of the file must be done or not
+	 */
+	public static boolean getValUpdate() {
+		return ValUpdate;
+	}
 }
--- src/SocketService/TCPClientSocket.java
+++ src/SocketService/TCPClientSocket.java
@@ -5,21 +5,38 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-
 import Model.Course;
 
 public class TCPClientSocket{
+	/**
+	 * Contains the IP address of the socket server handled by the main application
+	 */
 	private String IPAddress = null;
-	private int comPort = 3007;
+	/**
+	 * Corresponds to the input port of the socket server 
+	 */
+	private int conPort = 3007;
+	/**
+	 * Represents the communication between the socket client and the socket server
+	 */
 	private Socket socket = null;
 	
+	/**
+	 * Constructor of the class, it has nothing to do 
+	 * 		the IP Address and communication port are fixed before the launch of the application
+	 */
 	public TCPClientSocket() {
 		
 	}
 
+	/**
+	 * @author Xavier Bouchenard
+	 * @param lCourse	List of courses 
+	 * Sends a list of courses of the current day to store for use
+	 */
 	public void sendData(ArrayList<Course> lCourse) {
 		try {
-			socket = new Socket(IPAddress, comPort);
+			socket = new Socket(IPAddress, conPort);
 			System.out.println("The socket connection succeed");
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
@@ -38,15 +55,20 @@ public class TCPClientSocket{
 			System.out.println("Failed to give information of the courses to the server.");
 		}
 		
-		closeSocket();
+		closeSocketConnection();
 	}
 
-	private void closeSocket() {
+	/**
+	 * @author Xavier Bouchenard
+	 * Closes the socket connection
+	 */
+	private void closeSocketConnection() {
 		try {
 			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("Failed to close the socket connection with the server.");
 		}
+		socket = null;
 	}
 }
