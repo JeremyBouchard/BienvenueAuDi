d3971558dce750f7cfa8fe759d80882d14a75846
Mariam Konate <mariamko25@hotmail.com>
2018-04-09 15:24:16 +0200

    nouvelles classes client

--- main/Direction.java
+++ main/Direction.java
@@ -1,4 +1,30 @@
-package main;
-public enum Direction {
+package projectModel;
+/**
+ *  Enumeration that lists all the possible direction
+ *  that could be taken.
+ *  @author Aymane RAMACH, Michel NGATIMO, Morad SANBA
+ */
 
+public enum Direction
+{
+    None("NONE"),
+    North("NORTH"),
+    South("SOUTH"),
+    East("EAST"),
+    West("WEST");
+
+    private final String name;
+
+    private Direction(String s) {
+        name = s;
+    }
+
+    public boolean equals(String otherName)
+    {
+        return name.equals(otherName);
+    }
+
+    public String toString() {
+        return this.name;
+    }
 }
