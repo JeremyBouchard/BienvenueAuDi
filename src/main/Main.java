84f37247f9794ffec821037d86af047b48bcb635
JermanKell <xbouchenard@gmail.com>
2018-05-29 07:39:00 +0200

    New classes diagram

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,11 +2,18 @@ package main;
 
 import controllers.TimeHandler;
 
+/**
+ * Main of the Sources Application
+ * @author Xavier
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		ADEController ade = new ADEController();		
+		ADEController ade = new ADEController();	
+		// Starts the system
 		ade.launch();
+		// Starts the time handler
 		new Thread(new TimeHandler()).start();
 	}
 }
